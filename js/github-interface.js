a93f8cc838d08ac58e70c8b7dd380e99efdc97cc
Jackson Meyer <Guest@epicodus-42.local>
2016-10-14 14:35:47 -0700

    add a forLoop that iterates through my repositories and lists them out on the webpage

--- js/github-interface.js
+++ js/github-interface.js
@@ -3,41 +3,46 @@ var apiKey = require('./../.env').apiKey;
 
 
 $(document).ready(function() {
-    // var newUser = new User("string")
+  // var newUser = new User("string")
   $('#usernameForm').submit(function(event){
     event.preventDefault();
     var username = $('#username').val();
-    console.log(username);
 
+    // getting the name and bio from the username that was inputted
 
-// getting the name and bio from the username that was inputted
-
-      $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-        console.log(response);
-        $('#name').text("Name: " + (response.name));
-        $('#bio').text("Bio: " + (response.bio));
-      }).fail(function(error){
-        console.log(error.responseJSON.message);
-      });
-
-// getting the time each one was created at, and repos
-      var repoName = [];
-      var timeCreated = [];
-      $.get('https://api.github.com/users/' + username + '/repos' + '?access_token=' + apiKey).then(function(response) {
-        console.log(response);
-
-        for(var i = 0; i< response.length; i++) {
-          repoName.push(response[i].name);
-          timeCreated.push(response[i].created_at);
-        }
-        console.log(repoName)
-      });
+    $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+      console.log(response);
+      $('#name').text("Name: " + (response.name));
+      $('#bio').text("Bio: " + (response.bio));
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
     });
-  });
 
-  // $('.RepoName').text(username + "list of repo's, and when they were created can be seen below") {
-  //   var counter = 0;
-  //   repoName.forEach(function(repoName) {
-  //     $('.RepoName').append(<li>"response[] + "created at:" +
-  //   })
-  // }
+    // getting the time each one was created at, and repos
+    var repoNames = [];
+    var timeCreated = [];
+    $.get('https://api.github.com/users/' + username + '/repos' + '?access_token=' + apiKey).then(function(response) {
+      console.log(response);
+
+      for(var i = 0; i< response.length; i++) {
+        repoNames.push(response[i].name);
+        timeCreated.push(response[i].created_at);
+      }
+
+
+      $('.repoName').text(username + " 's'" + " list of repo's, and when they were created can be seen below"); {
+        var counter = 0;
+
+        repoNames.forEach(function(repoNames) {
+          console.log(repoNames);
+          console.log(timeCreated);
+          $('.repoName').append("<li>" + repoNames + "</li>");
+          counter++;
+        });
+      }
+    });
+
+
+
+  });
+});
